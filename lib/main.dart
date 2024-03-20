f6e8d1a1ad1ad05e23e3f5b894f42ce7ccc70fc9
이은재 <>
2024-03-20 11:01:42 +0900

    기본적인 AppBar 구성, 사용 시 팀원들이 자신의 화면에 맞게 수정 필요. ex) padding, action, 등등

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "테스트",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            backgroundColor: Colors.white,
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ));
   }
 }
