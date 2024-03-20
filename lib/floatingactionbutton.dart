82797fb37f15b36a8023bcd772cf8df1c5e8e50c
ganggwonlee@gmail.com <ganggwonlee@gmail.com>
2024-03-20 13:16:09 +0900

    FloatingActionButton commit

---
+++ lib/floatingactionbutton.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: Scaffold(
+//           floatingActionButton: FloatingActionButton(
+//             onPressed: () {},
+//             child: const Icon(Icons.add),
+//           ), // This trailing comma makes auto-formatting nicer for build methods.
+//         ));
+//   }
+// }
