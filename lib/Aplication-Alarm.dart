92abe3f936ac84f745965e1c11617cefa6b93cbe
ganggwonlee@gmail.com <ganggwonlee@gmail.com>
2024-03-22 14:35:07 +0900

    Aplication-Alarm commit

--- lib/Aplication-Alarm.dart
+++ lib/Aplication-Alarm.dart
@@ -1,5 +1,7 @@
 // import 'package:flutter/material.dart';
 //
+// import 'notification.dart';
+//
 // void main() {
 //   runApp(const MyApp());
 // }
@@ -10,8 +12,45 @@
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(),
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const HomePage(),
+//     );
+//   }
+// }
+//
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+//
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   void initState() {
+//     // 초기화
+//
+//     FlutterLocalNotification.init();
+//
+//     // 3초 후 권한 요청
+//
+//     Future.delayed(const Duration(seconds: 3),
+//         FlutterLocalNotification.requestNotificationPermission());
+//
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: TextButton(
+//           onPressed: () => FlutterLocalNotification.showNotification(),
+//           child: const Text("알림 보내기"),
+//         ),
+//       ),
 //     );
 //   }
 // }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'notification.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,45 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    // 초기화
-
-    FlutterLocalNotification.init();
-
-    // 3초 후 권한 요청
-
-    Future.delayed(const Duration(seconds: 3),
-        FlutterLocalNotification.requestNotificationPermission());
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () => FlutterLocalNotification.showNotification(),
-          child: const Text("알림 보내기"),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
